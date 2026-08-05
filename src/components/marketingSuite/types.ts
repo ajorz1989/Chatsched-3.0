e6a5c9ecb830c19e73fd763dc7a8543773b13de6
ajorz1989 <ajoranzie@gmail.com>
2026-08-05 15:36:47 +0200

    Delete src/components/marketingSuite/types.ts

--- src/components/marketingSuite/types.ts
+++
@@ -1,4 +0,0 @@
-import type { Database } from '../../types/database';
-
-export type Campaign = Database['public']['Tables']['campaigns']['Row'];
-export type CampaignInsert = Database['public']['Tables']['campaigns']['Insert'];
